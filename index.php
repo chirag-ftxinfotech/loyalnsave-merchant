69033767ae4a97deef8bb1cef7474056031c3625
ftxPrakashKumar <sudhir.virpara@ftxinfotech.com>
2021-09-07 12:34:48 +0530

    worked on home page

--- index.php
+++ index.php
@@ -26,12 +26,58 @@
         </div>
     </section>
 
+    <section class="animated-row pt-5 ">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                <svg 
+ xmlns="http://www.w3.org/2000/svg"
+ xmlns:xlink="http://www.w3.org/1999/xlink"
+ width="546.5px" height="727.5px">
+
+<path fill-rule="evenodd"  stroke="rgb(236, 119, 171)" stroke-width="3px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)"
+ d="M11.421,1.593 C16.435,1.593 20.499,5.672 20.499,10.702 C20.499,15.733 16.435,19.812 11.421,19.812 C6.408,19.812 2.343,15.733 2.343,10.702 C2.343,5.672 6.408,1.593 11.421,1.593 Z"/>
+
+<path fill-rule="evenodd"  stroke-width="3px" stroke-dasharray="12, 6" stroke-linecap="butt" stroke-linejoin="miter" fill="none"
+ d="M4.499,1.499 L4.642,85.577 C4.642,105.669 20.908,121.957 40.974,121.957 L318.507,122.121 "/>
+
+</svg>
+                    <ul class="section-card pl-0 d-flex flex-wrap">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 header-style-2 ">
+                        <h3>Lorem Ipsum</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing
+                            and typesetting industry. Lorem Ipsum has been the
+                            industry's standard dummy text ever since the 1500
+                            when an unknown printer took a galley of type and
+                            scrambled it to make a type specimen book. 
+
+                            Lorem Ipsum is simply dummy text of the printing
+                            and typesetting industry. Lorem Ipsum has been
+                            the industry'.</p>
+
+                            <a href="#" class="button">LEARN MORE ABOUT US</a>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
     <section class="feature-section animated-row">
         <svg class="scene" preserveAspectRatio="none" viewBox="0 0 1440 800">
             <defs><linearGradient id="MyGradient" x2="0" y2="1"><stop offset="0%" stop-color="#e3eeff" /><stop offset="100%" stop-color="#f3e7e9" /></linearGradient></defs>
             <path style="fill:url(#MyGradient);" d="M 1066,436 C 1051,543.8 973.2,656.2 873.6,700.1 756.6,751.7 600.9,725 492.4,657.4 431.5,619.5 387.5,546.9 376.7,476 360.3,368.3 377.9,229.2 462.5,160.5 589.5,57.34 815.4,42.24 952.4,131.7 1044,190.8 1081,328.8 1066,436 Z" pathdata:id="M 1041,450.4 C 1023,547.7 992.8,667.7 905.7,714.5 793.1,775 639,728.7 524.5,671.8 453.3,636.4 382.2,575.4 360.2,499 329.7,393 344.6,249.2 426,174.9 568.6,44.66 851.1,-8.71 1002,111.8 1091,182.7 1061,338.6 1041,450.4 Z;M 1066,436 C 1051,543.8 976.5,664.5 873.6,700.1 761,739.1 636.4,655.8 529.5,603.1 441.6,559.8 325.8,520.1 293.8,427.5 263.1,338.4 294.5,213.4 368.2,154.8 520.7,33.48 790.1,23.76 952.4,131.7 1043,191.7 1081,328.8 1066,436 Z;M 1066,436 C 1053,531.1 930.7,580.1 842.2,617.2 734,662.7 598.4,707.8 492.4,657.4 427.6,626.6 387.5,546.9 376.7,476 360.3,368.3 376.9,227.9 462.5,160.5 567.6,77.69 749.9,37.5 863.8,148.8 947.6,230.7 1082,320.1 1066,436 Z"/>
         </svg>
         <div class="container">
+
+        
+
+
             <div class="header-style-2 text-center animate" data-animate="fadeInUp">
                 <h3>Fully Customizable Loyalty Program</span></h3>
 				<h5>Your Business. Your Customers. Your Plan.</h5>
