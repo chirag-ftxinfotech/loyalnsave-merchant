d11aca03cddd09c59e01f6a7dc82718561f6ab06
Prakash Kumar <prakash.kumar@ftxinfotech.com>
2021-10-06 12:33:37 +0530

    worked on animation

--- pricing.php
+++ pricing.php
@@ -6,7 +6,7 @@
 
     <main id="content">
 
-        <section class="header-style-1 header-style-1-inner">
+        <section class="header-style-1 header-style-1-inner animated-row">
             <div class="anim-circle anim-circle--one">
                 <img src="assets/img/new-images/header-circle.svg" width="150" height="150" />
             </div>
@@ -22,7 +22,7 @@
             <div class="anim-circle anim-circle--five">
                 <img src="assets/img/new-images/header-circle.svg" width="150" height="150" />
             </div>
-            <div class="container">
+            <div class="container animate" data-animate="fadeInLeft">
                 <div class="row align-items-center header-inner-content">
                     <div class="col-12 col-lg-5">
                         <hgroup class="header__title">
@@ -38,10 +38,10 @@
             </div>
         </section>
 
-        <section class="section bg-dot-right-top bg-dot-left-top">
+        <section class="section bg-dot-right-top bg-dot-left-top animated-row">
             <div class="container container-1600 bg-dot-left-bottom">
 
-                <div class="header-style-2 text-center">
+                <div class="header-style-2 text-center animate" data-animate="fadeInUp">
                     <h3 class="sub-title">Pricing Plan</h3>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
                         the industry's standard dummy text ever since the 1500 when an unknown .</p>
@@ -51,7 +51,7 @@
                     <div class="row">
                         <div class="col-12">
 
-                            <div class="pricing-plans text-center">
+                            <div class="pricing-plans text-center animate" data-animate="fadeInUp">
                                 <div class="pricing-plan pp1">
                                     <div class="icon"><img src="http://placehold.it/50" alt="" /></div>
                                     <div class="price">$<strong>125</strong>/month</div>
@@ -82,11 +82,11 @@
             </div>
         </section>
 
-        <section class="section curved-section curved-theme pb-0">
+        <section class="section curved-section curved-theme pb-0 animated-row">
             <div class="curved-inner">
                 <div class="container container-1600">
 
-                    <div class="header-style-2 text-center">
+                    <div class="header-style-2 text-center animate" data-animate="fadeInUp">
                         <h3 class="sub-title">Frequently Asked Questions</h3>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
                             been the industry's standard dummy text ever since the 1500 when an unknown. Lorem Ipsum is
@@ -96,12 +96,13 @@
 
                     <div class="section-content">
                         <div class="row">
-                            <div class="col-12 col-lg-5 text-center order-lg-2">
-                                <img src="assets/img/new-images/faq.png" width="260" height="550" class="img-fluid" alt="" />
+                            <div class="col-12 col-lg-5 text-center order-lg-2 animate" data-animate="fadeInRight">
+                                <img src="assets/img/new-images/faq.png" width="260" height="550" class="img-fluid"
+                                    alt="" />
                             </div>
                             <div class="col-12 col-lg-5 offset-lg-1 order-lg-1">
 
-                                <div class="faq-accordion accordion" id="accordion">
+                                <div class="faq-accordion accordion animate" id="accordion" data-animate="fadeInUp">
 
                                     <div class="accordion-item active">
                                         <h5 class="accordion-title">Lorem Ipsum Dolor Sit</h5>
@@ -161,65 +162,6 @@
                 </div>
             </div>
         </section>
-
-        <section class="clientsay-section animated-row">
-
-            <div class="container">
-                <div class="row">
-                    <div class="header-style-2 text-center animate col-12">
-                        <h3 class="sub-title">What Our Clients Say</span></h3>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                            been the industry's
-                            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                            scrambled
-                            it to make a type specimen book. It has survived not only five centuries, but also the leap
-                            into
-                            electronic typesetting, remaining essentially unchanged.</p>
-
-                    </div>
-                </div>
-                <div class="client-slick">
-                    <div class="row align-items-center d-flex">
-                        <div class="col-md-6 text-center">
-                            <div class="client-thumb">
-                                <img src="assets/img/new-images/client-logo.png" width="155" height="113"
-                                    class="img-fluid" alt="">
-                            </div>
-                        </div>
-                        <div class="col-md-6 client-slick-right">
-                            <p>We knew that opening a restaurant meant we needed to have
-                                a loyalty program. Loyal~n~Save gave us the tools to get people
-                                into our place during those slow week nights. Plus, the detail
-                                view of our customer analytics is super helpful when identifying
-                                top customers.</p>
-                            <div class="client-slick-title">MUNCHIES</div>
-
-                        </div>
-                    </div>
-                    <div class="row align-items-center d-flex">
-                        <div class="col-md-6 text-center">
-                            <div class="client-thumb">
-                                <img src="assets/img/new-images/client-logo.png" width="155" height="113"
-                                    class="img-fluid" alt="">
-                            </div>
-                        </div>
-                        <div class="col-md-6 client-slick-right">
-                            <p>We knew that opening a restaurant meant we needed to have
-                                a loyalty program. Loyal~n~Save gave us the tools to get people
-                                into our place during those slow week nights. Plus, the detail
-                                view of our customer analytics is super helpful when identifying
-                                top customers.</p>
-                            <div class="client-slick-title">MUNCHIES</div>
-
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
-
-        </section>
-
     </main>
     <?php include('footer.php'); ?>
     <script>
